f3cbb2e6c4e13133b0b3a6843255f28c0a1c66d9
facu <ribettif@gmail.com>
2020-11-09 20:35:25 -0300

    terminado de agregar el modelo

---
+++ src/main/java/ar/edu/ucc/arqSoft/segundoParcial/model/Project.java
@@ -0,0 +1,89 @@
+package ar.edu.ucc.arqSoft.segundoParcial.model;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import ar.edu.ucc.arqSoft.common.model.GenericObject;
+
+@Entity
+@Table(name = "PROJECT")
+public class Project extends GenericObject {
+
+	@ManyToMany(mappedBy = "projects")
+	private Set<User> users;
+	
+	public Set<User> getUsers() {return users;};	
+	
+	@NotNull
+	@Size(min = 1, max = 400)
+	@Column(name = "NAME")
+	private String name;
+	
+	@NotNull
+	@Size(min = 10, max = 5000)
+	@Column(name = "DESCRIPTION")
+	private String description;
+	
+	@NotNull
+	@Column(name = "START")
+	private Date start;
+	
+	@NotNull
+	@Column(name = "FINISH")
+	private Date finish;
+	
+	@NotNull
+	@Column(name = "STATE") //deberia de ser una relacion con state?
+	private String state;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getFinish() {
+		return finish;
+	}
+
+	public void setFinish(Date finish) {
+		this.finish = finish;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+}
---
+++ src/main/java/ar/edu/ucc/arqSoft/segundoParcial/model/User.java
@@ -0,0 +1,160 @@
+package ar.edu.ucc.arqSoft.segundoParcial.model;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import ar.edu.ucc.arqSoft.common.model.GenericObject;
+
+@Entity
+@Table(name = "USER")
+public class User extends GenericObject {
+
+
+	@ManyToMany
+	 @JoinTable(
+	   name="USER_PROJECT",
+	   joinColumns=@JoinColumn(name="USER_ID", referencedColumnName="ID"),
+	   inverseJoinColumns=@JoinColumn(name="PROJECT_ID", referencedColumnName="ID"))
+	public Set<Project> projects ;
+	
+	public Set<Project> getProjects(){return projects;};
+
+	@NotNull
+	@Size(min = 1, max = 400)
+	@Column(name = "NAME")
+	private String name;
+	
+	@NotNull
+	@Size(min = 1, max = 400)
+	@Column(name = "LASTNAME")
+	private String lastName;
+	
+	@NotNull
+	@Size(min = 1, max = 400)
+	@Column(name = "ADRESSS")
+	private String adress;
+	
+	@NotNull
+	@Size(min = 1, max = 400)
+	@Column(name = "EMAIL")
+	private String email;
+	
+	@NotNull
+	@Column(name = "PHONE")
+	private int phone;
+	
+	@NotNull
+	@Column(name = "DNI")
+	private int DNI;
+	
+	@NotNull
+	@Column(name = "INSCRIPTION")
+	private Date inscription;
+	
+	@NotNull
+	@Column(name = "BIRTHDAY")
+	private Date birthday;
+	
+	@NotNull
+	@Size(min = 1, max = 400)
+	@Column(name = "USERNAME")
+	private String username;
+	
+	@NotNull
+	@Size(min = 1, max = 400)
+	@Column(name = "PASSWORD")
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public int getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(int dNI) {
+		DNI = dNI;
+	}
+
+	public Date getInscription() {
+		return inscription;
+	}
+
+	public void setInscription(Date inscription) {
+		this.inscription = inscription;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setProjects(Set<Project> projects) {
+		this.projects = projects;
+	}
+}
