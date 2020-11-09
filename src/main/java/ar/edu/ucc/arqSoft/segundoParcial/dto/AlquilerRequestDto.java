d385375e22214cade1515935d06f6ba58b7bf556
Fran L <franluque2@gmail.com>
2020-11-09 17:44:19 -0300

    Agregados paquetes

---
+++ src/main/java/ar/edu/ucc/arqSoft/segundoParcial/dto/AlquilerRequestDto.java
@@ -0,0 +1,25 @@
+package ar.edu.ucc.arqSoft.segundoParcial.dto;
+
+public class AlquilerRequestDto {
+
+		private Long socioId;
+		
+		private Long peliculaId;
+
+		public Long getSocioId() {
+			return socioId;
+		}
+
+		public void setSocioId(Long socioId) {
+			this.socioId = socioId;
+		}
+
+		public Long getPeliculaId() {
+			return peliculaId;
+		}
+
+		public void setPeliculaId(Long peliculaId) {
+			this.peliculaId = peliculaId;
+		}
+		
+}
