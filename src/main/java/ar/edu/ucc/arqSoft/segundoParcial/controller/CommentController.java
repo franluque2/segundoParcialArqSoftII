507e9b10bf7b8d7c98ac8521baad417c10e8800b
Fran L <franluque2@gmail.com>
2020-11-22 01:25:31 -0300

    Agregados Controladores

---
+++ src/main/java/ar/edu/ucc/arqSoft/segundoParcial/controller/CommentController.java
@@ -0,0 +1,52 @@
+package ar.edu.ucc.arqSoft.segundoParcial.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ar.edu.ucc.arqSoft.common.exception.BadRequestException;
+import ar.edu.ucc.arqSoft.common.exception.EntityNotFoundException;
+import ar.edu.ucc.arqSoft.segundoParcial.dto.CommentRequestDto;
+import ar.edu.ucc.arqSoft.segundoParcial.dto.CommentResponseDto;
+import ar.edu.ucc.arqSoft.segundoParcial.exception.TaskClosedException;
+import ar.edu.ucc.arqSoft.segundoParcial.service.CommentService;
+
+@Controller
+@RequestMapping("/comment")
+public class CommentController {
+	
+	@Autowired
+	private CommentService commentService;
+	
+    @RequestMapping(method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody List<CommentResponseDto> getAllComments()
+    {
+        return commentService.getAllComments();
+    }
+    
+    @RequestMapping(value="/{id}", method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody CommentResponseDto getCommentById(@PathVariable("id") Long id) throws EntityNotFoundException, BadRequestException {
+    	return commentService.getCommentById(id);
+		//return null;
+    }
+    @RequestMapping(value="/task/{id}", method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody List<CommentResponseDto> getAllCommentsByTaskId(@PathVariable("id") Long id) throws EntityNotFoundException, BadRequestException {
+    	return commentService.getAllCommentsFromTask(id);
+		//return null;
+    }
+    
+    @RequestMapping(method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces= MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody CommentResponseDto saveComment(@RequestBody CommentRequestDto request) throws EntityNotFoundException, BadRequestException, TaskClosedException
+    {
+        return commentService.insertComment(request);
+    }
+    
+
+}
---
+++ src/main/java/ar/edu/ucc/arqSoft/segundoParcial/controller/TaskController.java
@@ -0,0 +1,65 @@
+package ar.edu.ucc.arqSoft.segundoParcial.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import ar.edu.ucc.arqSoft.common.exception.BadRequestException;
+import ar.edu.ucc.arqSoft.common.exception.EntityNotFoundException;
+import ar.edu.ucc.arqSoft.segundoParcial.dto.CommentRequestDto;
+import ar.edu.ucc.arqSoft.segundoParcial.dto.CommentResponseDto;
+import ar.edu.ucc.arqSoft.segundoParcial.dto.TaskChangeRequestDto;
+import ar.edu.ucc.arqSoft.segundoParcial.dto.TaskChangeStateRequestDto;
+import ar.edu.ucc.arqSoft.segundoParcial.dto.TaskRequestDto;
+import ar.edu.ucc.arqSoft.segundoParcial.dto.TaskResponseDto;
+import ar.edu.ucc.arqSoft.segundoParcial.exception.TaskClosedException;
+import ar.edu.ucc.arqSoft.segundoParcial.service.CommentService;
+import ar.edu.ucc.arqSoft.segundoParcial.service.TaskService;
+
+@Controller
+@RequestMapping("/task")
+public class TaskController {
+	
+	@Autowired
+	private TaskService taskService;
+	
+    @RequestMapping(method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody List<TaskResponseDto> getAllTasks()
+    {
+        return taskService.getAllTasks();
+    }
+    
+    @RequestMapping(value="/{id}", method=RequestMethod.GET, produces= MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody TaskResponseDto getTaskById(@PathVariable("id") Long id) throws EntityNotFoundException, BadRequestException {
+    	return taskService.getTaskById(id);
+		//return null;
+    }
+    
+    
+    @RequestMapping(method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces= MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody TaskResponseDto saveTask(@RequestBody TaskRequestDto request) throws EntityNotFoundException, BadRequestException, TaskClosedException
+    {
+        return taskService.insertTask(request);
+    }
+    
+    @RequestMapping(method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces= MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody TaskResponseDto changeTaskUser(@RequestBody TaskChangeRequestDto request) throws EntityNotFoundException, BadRequestException, TaskClosedException
+    {
+        return taskService.changeUser(request);
+    }
+    
+    @RequestMapping(method=RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces= MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody TaskResponseDto changeTaskState(@RequestBody TaskChangeStateRequestDto request) throws EntityNotFoundException, BadRequestException, TaskClosedException
+    {
+        return taskService.changeState(request);
+    }
+    
+
+}
